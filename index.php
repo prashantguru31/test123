0d603f6cedfbc9d3057e2daca29eb57d8af5f759
Prashant Channe <prashantguru31@gmail.com>
2020-07-10 11:03:00 +0530

    essage=new commi hghhgt

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php 
 
+echo "<h2><b><i><u>This Is My 1st ELK CI / CD Pipe Line </b></i></u></h2>";
+echo "<hr>";
+echo "<br>";
 echo phpinfo();
 
 ?>
--- labels.php
+++ labels.php
@@ -1,4 +1,7 @@
 <?php
+echo "<h2><b><i><u>This Is My 1st ELK CI / CD Pipe Line </b></i></u></h2>";
+echo "<hr>";
+echo "<br>";
 
 $msg = "";
 if (($_POST)){
